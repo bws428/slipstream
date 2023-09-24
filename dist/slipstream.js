19fa7e99650bd3bfde366da65593959ea6f4148a
Brian D. Wendt <briandwendt@gmail.com>
2023-09-24 08:42:30 -0400

    Organize elements in getFlights()

--- dist/slipstream.js
+++ dist/slipstream.js
@@ -166,17 +166,20 @@ function downloadCsv(csv, pairingNumber, pairingDate) {
  * @return {Array} A list of flight segments.
  */
 function getFlights(gridText) {
-  // Get value of "gGridText" var from input string and return an array of legs.
+  // Get value of "gGridText" var from input string and return an array of flight segments.
   const flights = gridText.split("'")[1].split('-').filter((entry) => entry.substring(0, 3).search('L:') !== -1).map((row) => row.split('::'));
 
   // Build an array of flight segment objects from the flights table.
   return flights.map((row) => {
+    // NK ship numbers are 4 digits, with the last 3 being the aircraft N-number.
     const tailNumber = row[18].trim().slice(-3);
+
+    // Grab the flight segment details and store as an object
     return {
-      dh: row[7].trim(),
       date: row[4].trim(),
       code: row[5].trim(),
       fltNum: row[6].trim().replace(/\b0+/g, ''),
+      dh: row[7].trim(),
       orig: row[8].trim(),
       dest: row[9].trim(),
       depart: row[10].trim(),
