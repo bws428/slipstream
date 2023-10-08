422d1d45c0092d5d31ab317148b624fccbb98ba7
Brian D. Wendt <briandwendt@gmail.com>
2023-10-08 09:23:38 -0400

    Rename webpack Entry Point to `index.js`

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/slipstream.js',
+  entry: './src/index.js',
   output: {
     filename: 'slipstream.js',
     path: path.resolve(__dirname, 'dist'),
