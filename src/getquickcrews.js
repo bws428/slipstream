94050e6ffa1dff652814c0c9821e9762edb801f8
Brian D. Wendt <briandwendt@gmail.com>
2023-10-08 21:08:43 -0400

    Comment cleanup

--- src/getquickcrews.js
+++ src/getquickcrews.js
@@ -29,20 +29,3 @@ export default function getQuickCrews(hdnCrewData) {
   // Return an array of objects, since that's what addCrews() expects.
   return [crewNames];
 }
-
-// HERE are the 10 fields associated with the hdnCrewData.....
-// IS the CA always listed FIRST though?  Looks like YES.
-// What is the SAFE way to grab this data?  Just by array position?
-// Or should we actually try to "read" it?
-// I mean, we do array position on all the other crap in this program.
-
-//   0. Notified 
-// 1. ===> Pos (crew role)
-//   2. Ver (Pairing version? i.e., "A" or "B")
-// 3. ===> Employee ID 
-// 4. ===> Last Name 
-// 5. ===> First Name 
-//   6. Sen. # 
-//   7. Languages 
-//   8. Commuter 
-//   9. Hotel 
