dc2148fbf0f0e835d35b2e4f9fc6d5de63dd8a55
Brian D. Wendt <briandwendt@gmail.com>
2023-10-09 20:15:16 -0400

    Add test-data.js for sharing between tests

--- tests/get-crew-urls.test.js
+++ tests/get-crew-urls.test.js
@@ -2,31 +2,18 @@
  * @jest-environment jsdom
  */
 import getCrewUrls from "../src/get-crew-urls";
+import { url1, url2, url3, crewUrls } from "./test-data";
 
 // Create some `div` elements to simulate possible input params.
 const div1 = document.createElement("div");
 const div2 = document.createElement("div");
 const div3 = document.createElement("div");
-
-const url1 = `parent.window.onDetailClick("ctw4130report.aspx?FltNo=0930&DeptDate=20230920&DisplayDeptDate=09/20/23&DeptCity=MCO&ArrvCity=RIC&Modal=ctwpm",2);`;
-const url2 = `parent.window.onDetailClick("ctw4130report.aspx?FltNo=0216&DeptDate=20230921&DisplayDeptDate=09/21/23&DeptCity=RIC&ArrvCity=LAS&Modal=ctwpm",2);`;
-const url3 = `parent.window.onDetailClick("ctw4130report.aspx?FltNo=3020&DeptDate=20230922&DisplayDeptDate=09/22/23&DeptCity=MCI&ArrvCity=MCO&Modal=ctwpm",2);`;
-
 div1.setAttribute("onclick", url1);
 div2.setAttribute("onclick", url2);
 div3.setAttribute("onclick", url3);
 
 // Create an array of `div` element(s).
 const menuItems = [ div1, div2, div3 ];
-
-// Create a matching output array of correct URL strings.
-const crewUrls =
-  [
-    `https://workspace.spirit.com/cvpn/https/ctweb.spirit.com/CrewWeb/ctw4130report.aspx?FltNo=0930&DeptDate=20230920&DisplayDeptDate=09/20/23&DeptCity=MCO&ArrvCity=RIC&Modal=ctwpm`,
-    `https://workspace.spirit.com/cvpn/https/ctweb.spirit.com/CrewWeb/ctw4130report.aspx?FltNo=0216&DeptDate=20230921&DisplayDeptDate=09/21/23&DeptCity=RIC&ArrvCity=LAS&Modal=ctwpm`,
-    `https://workspace.spirit.com/cvpn/https/ctweb.spirit.com/CrewWeb/ctw4130report.aspx?FltNo=3020&DeptDate=20230922&DisplayDeptDate=09/22/23&DeptCity=MCI&ArrvCity=MCO&Modal=ctwpm`
-  ];
-
 
 test('Returns a properly-formatted crewUrls array.', () => {
   expect(getCrewUrls(menuItems)).toStrictEqual(crewUrls);
--- tests/get-crews.test.js
+++ tests/get-crews.test.js
@@ -1,31 +1,5 @@
 import getCrews from "../src/get-crews";
-
-const hdnCrewData = `Yes::CA::A::76148::   WENDT::BRIAN       ::1919 :: :: :: :-:
-                     Yes::FO::A::113947::  SCHLENK::SUSANNE   ::2870 :: :: ::`
-
-const crewNames = [
-  {
-    "fltNum": "",
-    "orig": "",
-    "dest": "",
-    "crew": [
-      {
-        "role": "CA",
-        "dh": "",
-        "id": "76148",
-        "last": "Wendt",
-        "first": "Brian"
-      },
-      {
-        "role": "FO",
-        "dh": "",
-        "id": "113947",
-        "last": "Schlenk",
-        "first": "Susanne"
-      }
-    ]
-  }
-]
+import { hdnCrewData, crewNames } from "./test-data";
 
 test('Returns a properly-formatted crewNames object.', () => {
   expect(getCrews(hdnCrewData)).toStrictEqual(crewNames);
